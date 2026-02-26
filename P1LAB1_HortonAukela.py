a6bed80453d5b6ec7a745b34cc2e01ccf3b2505b
AukelaH <aukelahorton22@gmail.com>
2026-02-26 01:20:28 -0500

    Add files via upload

---
+++ P1LAB1_HortonAukela.py
@@ -0,0 +1,9 @@
+# Aukela Horton
+# 02/26/2026
+# P1LAB1 - Input and Output Basics
+# This program asks the user for their first and last name and prints a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome to CTI-110,", first_name, last_name + "!")
